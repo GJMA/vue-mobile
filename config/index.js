9f351cd69eafdcafb0947d954faf28759fe188e9
zhangwenmin <zwm_gjma@sina.com>
2019-03-06 11:19:38 +0800

    修改IP

--- config/index.js
+++ config/index.js
@@ -14,7 +14,7 @@ module.exports = {
 
     // Various Dev Server settings
 //  host: '192.168.0.60', // can be overwritten by process.env.HOST
-    host: '10.130.27.169', // can be overwritten by process.env.HOST
+    host: '10.130.26.107', // can be overwritten by process.env.HOST
     port: 8082, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
